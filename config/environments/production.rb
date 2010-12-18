0e190f1d090dcab88579db786e2fe8c82aefba05
Alexey Kisel <aki@jetthoughts.com>
2010-12-19 01:17:13 +0200

    config update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,8 @@ TimeManage::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
- config.action_mailer.raise_delivery_errors = false
-config.action_mailer.default_url_options = { :host => "timereports.heroku.com"}
+ #config.action_mailer.raise_delivery_errors = false
+#config.action_mailer.default_url_options = { :host => "timereports.heroku.com"}
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 end
