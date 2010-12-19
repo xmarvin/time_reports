268aae93f5d0b3edbc2eb1246834b08f72578b4d
Alexey Kisel <aki@jetthoughts.com>
2010-12-19 14:20:50 +0200

    update notifier

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,28 +1,17 @@
 class Notifier < ActionMailer::Base
-  default :from => "donotreply@filerepository.heroku.com"
-
-  def new_file
-    mail(:to => "Spy-A@yandex.ru", :subject => "New File") do |format|
-      format.text {
-        render :text => "Hello"
-      }
-    end
-  end
+  default :from => "donotreply@timereports.heroku.com"
 
   def activation_instructions(user)
     @account_activation_url = activation_url(user.perishable_token)
     mail(:to => user.email, :subject => "Activation Instructions") do |format|
       format.html
-		end  
+		end
   end
 
   def activation_confirmation(user)
-    subject       "Activation Complete"
-    from          "noreply@time_reports.heroku.com"
-    recipients    user.email
-    sent_on       Time.now
-    body          :user => user
-    content_type "text/html"
+    mail(:to => user.email, :subject => "Activation Complete") do |format|
+      format.html
+		end
   end
 
   def password_reset_instructions(user)  
@@ -34,13 +23,11 @@ class Notifier < ActionMailer::Base
     content_type "text/html"
   end
 
-  def invite_was_sended(invite)  
-    subject       "You get invite"  
-    from          "TimeReports"
-    recipients    invite.email  
-    sent_on       Time.now
-    body :invite => invite
-    content_type "text/html"
+  def invite_was_sended(invite)
+    @invite = invite
+    mail(:to => invite.email, :subject => "You get invite") do |format|
+      format.html
+		end
   end
   
 end
